41246e7f1d8b872c3fa5b3c4aeb9524d9a1e064e
jmspiewak <j.m.spiewak@gmail.com>
2024-02-01 00:38:18 +0100

    Add virt dep as a submodule
    Crates.io currently has an old and buggy version.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use virt::connect::Connect;
+use virt::error::Error;
+
+const URI: &str = "qemu:///system";
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        println!("{e}");
+    }
+}
+
+fn run() -> Result<(), Error> {
+    let conn = Connect::open(Some(URI))?;
+    let doms = conn.list_all_domains(0)?;
+
+    for dom in doms {
+        let name = dom.get_name()?;
+        let info = dom.get_info()?;
+        let sched = dom.get_scheduler_parameters()?;
+        println!("{name}:\n  {info:?}\n  {sched:?}");
+    }
+
+    Ok(())
 }
